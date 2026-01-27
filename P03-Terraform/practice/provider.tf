2810d0369c1d37da0d8e19f601a5f6906bade630
Firdose <pfirdose1988@gmail.com>
2026-01-27 08:24:09 +0530

    updates

--- P03-Terraform/practice/provider.tf
+++ P03-Terraform/practice/provider.tf
@@ -19,7 +19,8 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami_id = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    vpc_security_group_ids = var.vpc_id
+    instance_type = var.environment == "dev" ? "t3.micrp": "t2.micro" 
+    vpc_security_group_ids = [aws_security_group.aws_security_group_name.id]
     provider = aws.us-east
   
 }
@@ -29,5 +30,25 @@ resource "aws_instance" "example" {
     instance_type = "t2.micro"
     vpc_security_group_ids = var.vpc_id
     provider = aws # default or no need to specify provider here 
+    tags = {
+      Name = "terraform"
+    }
+}
+
+resource "aws_security_group" "name" {
+    name = "allow ssh"
+    description = "allow all"
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+    }
+  
+}
+
+output "public_ip" {
+  value = aws
   
 }
